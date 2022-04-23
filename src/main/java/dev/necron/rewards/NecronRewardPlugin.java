38bbb87bdc2f42ac300c5f5a80af183d7abd9f44
Hakan Kargın <hakankargin03@gmail.com>
2022-04-23 17:36:07 +0300

    Add hCore to dependencies

--- src/main/java/dev/necron/rewards/NecronRewardPlugin.java
+++ src/main/java/dev/necron/rewards/NecronRewardPlugin.java
@@ -1,12 +1,13 @@
 package dev.necron.rewards;
 
+import com.hakan.core.HCore;
 import dev.necron.core.NecronPlugin;
 
 public class NecronRewardPlugin extends NecronPlugin {
 
     @Override
     public void whenEnabled() {
-
+        HCore.initialize(this);
     }
 
     @Override
